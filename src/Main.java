05f8956a058b0dc49a36e350e4ddfdd6d3cbf2ab
leviisac <45822362+leviisac@users.noreply.github.com>
2021-04-29 15:37:46 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,7 @@ import primitives.*;
 import static java.lang.System.out;
 import static primitives.Util.*;
 
-/**
- * Test program for the 1st stage
- *
- * @author Dan Zilberstein
- */
+
 public final class Main {
 
     /**
